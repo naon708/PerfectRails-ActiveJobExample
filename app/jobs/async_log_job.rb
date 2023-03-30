26cd451e9195614c6f710fa07a6f87e004f094e9
naon708 <naoya_nis@icloud.com>
2023-03-30 22:56:03 +0900

    引数で渡された値によって追加するキューを決められる書き方を試した

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,6 +1,18 @@
 class AsyncLogJob < ApplicationJob
   # 「このジョブクラスはasync_logというキューを使ってください」という指定
-  queue_as :async_log
+  # queue_as :async_log
+
+  # Blockにすることでジョブの内容によってキューを選ぶようにもできる
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    when "to another_queue"
+      :another_queue
+    else
+      :default
+    end
+  end
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)
